b5dd9dbf0c3cc5aa8e486ba1d5d364c013614471
John321Blacksmith <kuzmichevi321@gmail.com>
2025-05-04 18:21:05 +0300

    feat: http server on top of the go standart library

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -1,72 +1,48 @@
 package httpserver
 
 import (
+	"net/http"
 	"time"
 
-	"github.com/goccy/go-json"
-	"github.com/gofiber/fiber/v2"
+	"media_api/config"
 )
 
-type Server struct {
-	App    *fiber.App
-	notify chan error
-
-	address         string
-	prefork         bool
-	readTimeout     time.Duration
-	writeTimeout    time.Duration
-	shutdownTimeout time.Duration
+type HttpServer struct {
+	implementation *http.Server
+	address        string
+	readTimeout    time.Duration
+	writeTimeout   time.Duration
 }
 
 const (
-	_defaultAddr            = ":8000"
-	_defaultreadTimeout     = 5 * time.Second
-	_defaultwriteTimeout    = 5 * time.Second
-	_defaultshutdownTimeout = 5 * time.Second
+	_defaultAddr         = ":8000"
+	_defaultreadTimeout  = 5 * time.Second
+	_defaultwriteTimeout = 5 * time.Second
 )
 
-func New(opts ...Option) *Server {
-	s := &Server{
-		App:             nil,
-		notify:          make(chan error, 1), // ?
-		address:         _defaultAddr,
-		readTimeout:     _defaultreadTimeout,
-		writeTimeout:    _defaultwriteTimeout,
-		shutdownTimeout: _defaultshutdownTimeout,
+func New(cfg *config.HTTP) *HttpServer {
+	server := &HttpServer{
+		address:      cfg.Host + ":" + cfg.Port,
+		readTimeout:  _defaultreadTimeout,
+		writeTimeout: _defaultwriteTimeout,
 	}
 
-	app := fiber.New(
-		fiber.Config{
-			Prefork:      s.prefork,
-			ReadTimeout:  s.readTimeout,
-			WriteTimeout: s.writeTimeout,
-			JSONEncoder:  json.Marshal,
-			JSONDecoder:  json.Unmarshal,
-		},
-	)
-
-	// apply server instance
-	// attributes assignment
-	for _, opt := range opts {
-		opt(s)
+	app := &http.Server{
+		Addr:         server.address,
+		ReadTimeout:  server.readTimeout,
+		WriteTimeout: server.writeTimeout,
 	}
-	///
-	s.App = app
 
-	return s
+	server.implementation = app
+
+	return server
 }
 
-func (s *Server) Start() {
-	go func() {
-		s.notify <- s.App.Listen(s.address)
-		close(s.notify)
-	}()
-}
-
-func (s *Server) Notify() <-chan error {
-	return s.notify
-}
-
-func (s *Server) Sutdown() error {
-	return s.App.ShutdownWithTimeout(s.shutdownTimeout)
+func (s *HttpServer) Start() error {
+	err := s.implementation.ListenAndServe()
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
 }
