b452958cb35b3dbcf1b033f6c85e8859e7440416
John321Blacksmith <kuzmichevi321@gmail.com>
2025-06-05 00:49:20 +0300

    feat: added main HAndler

--- internal/adapter/delivery/http/handling.go
+++ internal/adapter/delivery/http/handling.go
@@ -1,4 +1,20 @@
 package http
 
-type Handler struct {
+import (
+	film_handler "media_api/internal/adapter/delivery/http/handler"
+	"net/http"
+)
+
+type HttpHandler struct {
+	*film_handler.FilmHandler
 }
+
+func NewHttpHandler(
+	film_handler *film_handler.FilmHandler,
+) *HttpHandler {
+	return &HttpHandler{
+		film_handler,
+	}
+}
+
+func (h *HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
