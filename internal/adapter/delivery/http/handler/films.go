cce3a89a108a77ce25f400f2a5e3ea998921d86f
John321Blacksmith <kuzmichevi321@gmail.com>
2025-06-05 00:48:29 +0300

    feat: created Film Handler

--- internal/adapter/delivery/http/handler/films.go
+++ internal/adapter/delivery/http/handler/films.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"context"
+	entity "media_api/internal/entity"
+	usecase "media_api/internal/usecase"
+	"net/http"
+)
+
+type FilmHandler struct {
+	usecase *usecase.RentalUseCase
+}
+
+func New(uc *usecase.RentalUseCase) *FilmHandler {
+	return &FilmHandler{
+		usecase: uc,
+	}
+}
+
+func (h *FilmHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) ([]entity.Film, error) {
+	return h.usecase.GetFilmsList(context.Background())
+}
