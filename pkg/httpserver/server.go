1eeef2a66c6252a1b15a58467f8f3b5689886e87
John321Blacksmith <kuzmichevi321@gmail.com>
2025-06-05 00:50:51 +0300

    feat: global server startup

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -2,47 +2,16 @@ package httpserver
 
 import (
 	"net/http"
-	"time"
 
 	"media_api/config"
 )
 
-type HttpServer struct {
-	implementation *http.Server
-	address        string
-	readTimeout    time.Duration
-	writeTimeout   time.Duration
-}
-
-const (
-	_defaultAddr         = ":8000"
-	_defaultreadTimeout  = 5 * time.Second
-	_defaultwriteTimeout = 5 * time.Second
-)
-
-func New(cfg *config.HTTP) *HttpServer {
-	server := &HttpServer{
-		address:      cfg.Host + ":" + cfg.Port,
-		readTimeout:  _defaultreadTimeout,
-		writeTimeout: _defaultwriteTimeout,
+func New(cfg *config.HTTP) *http.Server {
+	server := &http.Server{
+		Addr:         cfg.Host + ":" + cfg.Port,
+		ReadTimeout:  cfg.ReadTimeout,
+		WriteTimeout: cfg.WriteTimeout,
 	}
-
-	app := &http.Server{
-		Addr:         server.address,
-		ReadTimeout:  server.readTimeout,
-		WriteTimeout: server.writeTimeout,
-	}
-
-	server.implementation = app
 
 	return server
-}
-
-func (s *HttpServer) Start() error {
-	err := s.implementation.ListenAndServe()
-	if err != nil {
-		return err
-	} else {
-		return nil
-	}
 }
