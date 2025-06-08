7e15943165f68fcbd6d3d2095c577494f04581e8
John321Blacksmith <kuzmichevi321@gmail.com>
2025-06-08 23:24:10 +0300

    feat: pool was deleted and switched to another driver

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -4,13 +4,13 @@ package postgres
 
 import (
 	"fmt"
-	"log"
 	"time"
 
-	"context"
+	"golang.org/x/exp/slog"
+
 	"database/sql"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
 )
 
 // default values
@@ -26,41 +26,7 @@ type Postgres struct {
 	connAttempts int
 	connTimeout  time.Duration
 
-	db   *sql.DB
-	Pool *pgxpool.Pool
-}
-
-// function to return
-// a new instance of the
-// Postgres Pool
-func NewPool(url string) (*Postgres, error) {
-	pg := &Postgres{
-		maxPoolSize:  _defaultMaxPoolSize,
-		connAttempts: _defaultConnectionAttempts,
-		connTimeout:  _defaultConnTimeout,
-	}
-
-	poolConfig, err := pgxpool.ParseConfig(url)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing DB-pool configs: %v", err)
-	}
-	poolConfig.MaxConns = int32(pg.maxPoolSize)
-
-	for pg.connAttempts > 0 {
-		pg.Pool, err = pgxpool.NewWithConfig(context.Background(), poolConfig)
-		if err != nil {
-			break
-		}
-		log.Printf("Trying to connect to Postgres, attempts left: %d", pg.connAttempts)
-
-		time.Sleep(pg.connTimeout)
-		pg.connAttempts--
-	}
-	if err != nil {
-		return nil, fmt.Errorf("postgres - NewPostgres - connAttempts == 0: %w", err)
-	}
-
-	return pg, nil
+	db *sql.DB
 }
 
 // this function returns
@@ -71,15 +37,13 @@ func NewDB(url string) (*sql.DB, error) {
 		return nil, fmt.Errorf("error occurred while opening the DataBase: %w", err)
 	}
 	err = db.Ping()
+	if err != nil {
+		slog.Info("PING: DB unavailable")
+	} else {
+		slog.Info("\033[32m:DB connection established \033[0m \n")
+	}
 
 	return db, err
-}
-
-// close active connection pool
-func (db *Postgres) ClosePool() {
-	if db.Pool != nil {
-		db.Pool.Close()
-	}
 }
 
 // close active DB connection
